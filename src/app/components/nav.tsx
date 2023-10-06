070a341de72ac9043984775831817ae95694b0c5
Mikku44 <104062911+Mikku44@users.noreply.github.com>
2023-10-06 22:57:06 +0700

    Update nav.tsx

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -62,7 +62,7 @@ function Navbar() {
                     <Link target="_blank" href="tel:0829429446">082-9429446</Link>
                 </div>
 
-                <div className=' sm:hidden group'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <div className='sm:hidden group w-6 h-6'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
                 <ul className='duration-150 gap-5 group-hover:grid hover:grid hidden bg-white w-full py-3 px-3 top-[3rem] left-0 absolute'>
