d961687de090abba3f917bdefb3ea64651d33a2f
Mikku44 <104062911+Mikku44@users.noreply.github.com>
2023-10-14 12:15:51 +0700

    Update page.tsx

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,18 +1,31 @@
+'use client'
+import { motion, useMotionValueEvent, useScroll } from "framer-motion"
+
+
+function Indicator() {
+    const { scrollYProgress } = useScroll()
+
+    return <motion.div  style={{ scaleX: scrollYProgress }} className="bg-[#D33666] w-screen h-2"/>
+}
 
 
 export default function Page({ params }: { params: { id: string } }) {
+
     return <>
+
+        <div className="fixed"><Indicator /></div>
         <div className="py-10">
             <div className="card-actions justify-center">
                 <div className="badge p-5 text-black text-md hover:text-white ring-[#F2C762] cursor-pointer hover:bg-[#F2C762]">Bussiness</div>
                 <div className="badge p-5 text-black text-md hover:text-white ring-[#F2C762] cursor-pointer hover:bg-[#F2C762]">Products</div>
             </div>
 
+
             <h1 className="text-5xl text-center py-10">BRANDING การตลาดสุดฉ่ำ!</h1>
-            <div className="px-48">
+            <div className="sm:px-48 px-10">
                 <div className="rounded-xl overflow-hidden"><img src="/images/cover2.png" alt="" /></div>
                 <div className="py-10">
-                        Lorem ipsum dolor sit amet consectetur a
+                    Lorem ipsum dolor sit amet consectetur a
                     dipisicing elit. Minus assumenda delectus, au
                     t at numquam quas quidem, ullam tempore similique
                     atque debitis? Inventore sed atque beatae! Nobis fugit, mo
@@ -20,7 +33,7 @@ export default function Page({ params }: { params: { id: string } }) {
                     quo totam sed sit, dolore et eos eligendi id vero a doloremque fuga
                     aliquam quos?
                     <br />
-                        Lorem ipsum dolor sit amet consectetur a
+                    Lorem ipsum dolor sit amet consectetur a
                     dipisicing elit. Minus assumenda delectus, au
                     t at numquam quas quidem, ullam tempore similique
                     atque debitis? Inventore sed atque beatae! Nobis fugit, mo
