278b9ec13ed0f234f7439a8a0659c520fe218db6
Mikku44 <104062911+Mikku44@users.noreply.github.com>
2023-10-17 13:31:45 +0700

    addNavigator

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -5,7 +5,7 @@ import { motion, useMotionValueEvent, useScroll } from "framer-motion"
 function Indicator() {
     const { scrollYProgress } = useScroll()
 
-    return <motion.div  style={{ scaleX: scrollYProgress }} className="bg-[#D33666] w-screen h-2"/>
+    return <motion.div style={{ scaleX: scrollYProgress }} className="bg-[#D33666] w-screen h-2" />
 }
 
 
@@ -14,6 +14,7 @@ export default function Page({ params }: { params: { id: string } }) {
     return <>
 
         <div className="fixed"><Indicator /></div>
+
         <div className="py-10">
             <div className="card-actions justify-center">
                 <div className="badge p-5 text-black text-md hover:text-white ring-[#F2C762] cursor-pointer hover:bg-[#F2C762]">Bussiness</div>
@@ -21,7 +22,41 @@ export default function Page({ params }: { params: { id: string } }) {
             </div>
 
 
-            <h1 className="text-5xl text-center py-10">BRANDING การตลาดสุดฉ่ำ!</h1>
+            <div className="flex justify-center py-10">
+                {/* navigator bar */}
+
+                <nav className="flex" aria-label="Breadcrumb">
+                    <ol className="inline-flex items-center space-x-1 md:space-x-3">
+                        <li className="inline-flex items-center">
+                            <a href="./" className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                                <svg className="w-3 h-3 mr-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+                                </svg>
+                                Content
+                            </a>
+                        </li>
+                        <li>
+                            <div className="flex items-center">
+                                <svg className="w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                                </svg>
+                                <a href="./" className="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">Blogs</a>
+                            </div>
+                        </li>
+                        <li aria-current="page">
+                            <div className="flex items-center">
+                                <svg className="w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                                </svg>
+                                <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">{params.id}</span>
+                            </div>
+                        </li>
+                    </ol>
+                </nav>
+
+                {/* / */}
+            </div>
+            <h1 className="sm:text-3xl md:text-5xl text-3xl text-center py-10">BRANDING การตลาดสุดฉ่ำ!</h1>
             <div className="sm:px-48 px-10">
                 <div className="rounded-xl overflow-hidden"><img src="/images/cover2.png" alt="" /></div>
                 <div className="py-10">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-// 'use client'
+'use client'
 // import { motion } from "framer-motion"
 import Image from 'next/image'
 import Navbar from './components/nav'
@@ -6,6 +6,7 @@ import Link from 'next/link'
 // import { useState } from 'react'
 import Footer from './components/footer'
 import Head from 'next/head'
+import { motion } from 'framer-motion'
 
 
 const review = [
@@ -40,6 +41,19 @@ const consults = [
   { id: '2', name: 'Roren', des: 'blanditiis temporibus laborum, adipisci sit aut autem.' },
 ]
 
+const blogs = [
+  { id: '1', title: 'title', description: 'name' },
+  { id: '2', title: 'title', description: 'name' },
+  { id: '3', title: 'title', description: 'name' },
+  { id: '4', title: 'title', description: 'name' },
+  { id: '5', title: 'title', description: 'name' },
+]
+
+const videos = [
+  { id: '1', src: 'https://www.youtube.com/embed/dcVFxqyxACI?si=A6HAhtVsSUap2dtz', description: 'name' },
+  { id: '2', src: 'https://www.youtube.com/embed/c6Xxcyflb3o?si=db6h-ZqKwgDGcc7', description: 'name' },
+  { id: '3', src: 'https://www.youtube.com/embed/apSobqg8BKQ?si=lt2L_MmNOwLF1uxm', description: 'name' },
+]
 export default function Home() {
 
   // const [count, setCount] = useState(0)
@@ -47,7 +61,7 @@ export default function Home() {
     <>
       {/* <motion.div initial={false} animate={{ x: 100 }} /> */}
       <Navbar />
-      <div className='absolute sm:block hidden top-[90%]  sm:-left t-10 -z-10'>
+      {/* <div className='absolute sm:block hidden top-[90%]  sm:-left t-10 -z-10'>
         <svg className='w-[30rem] sm:w-[60rem]' height="663" viewBox="0 0 703 663" fill="none" xmlns="http://www.w3.org/2000/svg" >
           <path fill-rule="evenodd" clip-rule="evenodd" d="M199.454 2.92203C279.775 -15.3187 339.711 70.1149 416.946 98.6863C494.048 127.209 592.553 103.304 644.027 167.383C703.092 240.912 717.201 346.332 687.906 435.999C657.722 528.382 580.504 597.873 490.991 635.787C404.172 672.56 306.079 670.549 218.921 634.64C134.943 600.042 72.2263 530.766 35.0215 447.928C-1.7947 365.956 -12.3364 273.409 18.8157 189.101C49.9672 104.795 111.792 22.83 199.454 2.92203Z" fill="#D33666" fill-opacity="0.5" />
         </svg>
@@ -81,7 +95,7 @@ export default function Home() {
         <svg className='w-[30rem] sm:w-[60rem]' height="663" viewBox="0 0 703 663" fill="none" xmlns="http://www.w3.org/2000/svg" >
           <path fill-rule="evenodd" clip-rule="evenodd" d="M199.454 2.92203C279.775 -15.3187 339.711 70.1149 416.946 98.6863C494.048 127.209 592.553 103.304 644.027 167.383C703.092 240.912 717.201 346.332 687.906 435.999C657.722 528.382 580.504 597.873 490.991 635.787C404.172 672.56 306.079 670.549 218.921 634.64C134.943 600.042 72.2263 530.766 35.0215 447.928C-1.7947 365.956 -12.3364 273.409 18.8157 189.101C49.9672 104.795 111.792 22.83 199.454 2.92203Z" fill="#F2C762" fill-opacity="0.5" />
         </svg>
-      </div>
+      </div> */}
 
       <main className="my-10 sm:mx-10 mx-5">
 
@@ -105,16 +119,11 @@ export default function Home() {
 
         {/* review */}
 
-        <div className="   p-10 my-10">
+        <div className="   sm:p-10 my-10">
           <h2 className="text-2xl font-bold text-center py-5 pt-24">รีวิวคอร์สและหนังสือ</h2>
-          <div className="flex justify-center gap-5" >
 
-            <a className="tab actived border-2 rounded-full">Coure review</a>
-            <a className="tab border-2 rounded-full">Books</a>
 
-          </div>
-
-          <div className="grid grid-flow-row sm:grid-flow-col my-10 gap-5 overflow-scroll  scrollbar-hide py-10">
+          <div className="grid grid-flow-col my-10 gap-5 overflow-scroll bg-slate-100 rounded-lg shadow-xl p-10 scrollbar-hide py-10">
 
             {review.map((post) => {
               return <div key={post.id}>
@@ -151,6 +160,13 @@ export default function Home() {
             })}
 
 
+
+
+          </div>
+          <div className="flex justify-center gap-5" >
+
+            <a className="tab actived border-2 rounded-full">Coure review</a>
+            <a className="tab border-2 rounded-full">Books</a>
 
           </div>
         </div>
@@ -256,112 +272,57 @@ export default function Home() {
 
 
         {/* contents */}
-        <div className="mt-10">
-          <h2 className="text-2xl font-bold text-center py-5 pt-24">Contents</h2>
-
-          <div className="grid gap-10 sm:flex justify-center">
-            <div className="card w-96 bg-base-100 shadow-xl">
-              <figure className="px-10 pt-10">
-                <div className="rounded-2xl overflow-hidden "><img src="https://www.choomcham.com/assets/images/84dfa30d.png" alt="course one" className="hover:saturate-50 rounded-2xl hover:scale-110 duration-150" /></div>
-              </figure>
-              <div className="card-body ">
-                <h2 className="card-title">Online course</h2>
-                <p>รู้เรื่องธุรกิจใน 3 นาที</p>
-                <div className="card-actions">
-                  <button className="btn bg-primary">Buy Now</button>
+        <div className="px-10 py-20">
+                <h4 className="py-10 px-5 text-xl font-bold">ACITIVITY & EVENT</h4>
+                <div className="grid sm:grid-cols-3 gap-10 ">
+                    {blogs.map((blog) => {
+                        return (
+                            <>
+                                <motion.div initial={{ y: 20, opacity: 0 }}
+                        whileInView={{ y: 0, opacity: 100 }}  className="card sm:w-96 bg-base-100 shadow-xl">
+                                    <figure className="rounded-xl overflow-hidden"><img className="rounded-xl" src="https://www.choomcham.com/assets/images/84dfa30d.png" alt="Shoes" /></figure>
+                                    <div className="card-body">
+                                        <h2 className="card-title">
+                                            BRANDING การตลาดสุดฉ่ำ!
+                                            <div className="badge badge-secondary">NEW</div>
+                                        </h2>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique quod facilis hic eos!</p>
+                                        <div className="card-actions justify-end">
+                                            <div className="badge p-3 text-slate-400 hover:text-white ring-[#F2C762] cursor-pointer hover:bg-[#F2C762]">Bussiness</div>
+                                            <div className="badge p-3 text-slate-400 hover:text-white ring-[#F2C762] cursor-pointer hover:bg-[#F2C762]">Products</div>
+                                        </div>
+                                        <Link href={"./blog/" + (blog.id).toString()} className="btn">อ่านต่อ</Link>
+                                    </div>
+                                </motion.div>
+                            </>
+                        )
+                    })}
                 </div>
-              </div>
             </div>
-
-            {/*  */}
-
-            <div className="card w-96 bg-base-100 shadow-xl">
-              <figure className="px-10 pt-10">
-                <div className="rounded-2xl overflow-hidden "><img src="https://www.choomcham.com/assets/images/84dfa30d.png" alt="course two" className="hover:saturate-50 rounded-2xl hover:scale-110 duration-150" /></div>
-              </figure>
-              <div className="card-body ">
-                <h2 className="card-title">Online course</h2>
-                <p>รู้เรื่องธุรกิจใน 3 นาที</p>
-                <div className="card-actions">
-                  <button className="btn bg-primary">Buy Now</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
 
 
         {/* video content */}
-        <div className="mt-10">
-          <h2 className="text-2xl font-bold text-center py-5 pt-24">Videos</h2>
+        <h3 className="text-4xl p-10 text-center grid justify-items-center ">
+                วีดีโอความรู้สุดฉ่ำ
+                <div className="rounded-full h-1 w-48 bg-[#D33666]"></div>
+            </h3>
+            <div className="px-10 py-20">
+                <h4 className="py-5 px-5 text-xl font-bold">ChoomCham Branding Videos</h4>
 
-          <div className="grid gap-10 sm:flex justify-center">
-            <div className="card w-96 bg-base-100 shadow-xl">
-              <figure className="px-10 pt-10">
-                <div className="rounded-2xl overflow-hidden "><img src="https://www.choomcham.com/assets/images/84dfa30d.png" alt="course one" className="hover:saturate-50 rounded-2xl hover:scale-110 duration-150" /></div>
-              </figure>
-              <div className="card-body ">
-                <h2 className="card-title">Online course</h2>
-                <p>รู้เรื่องธุรกิจใน 3 นาที</p>
-                <div className="card-actions">
-                  <button className="btn bg-primary">Buy Now</button>
+                <div className="grid sm:grid-cols-3 gap-10">
+
+                    {videos.map((video) => <>
+                        <motion.div initial={{ y: 20, opacity: 0 }}
+                        whileInView={{ y: 0, opacity: 100 }}  className="card sm:w-96 overflow-hidden shadow-xl ">
+                            <figure className="h-48"><iframe width="560" src={video.src} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" className="object-cover h-full"></iframe></figure>
+                        </motion.div></>)}
                 </div>
-              </div>
             </div>
-
-            {/*  */}
-
-            <div className="card w-96 bg-base-100 shadow-xl">
-              <figure className="px-10 pt-10">
-                <div className="rounded-2xl overflow-hidden "><img src="https://www.choomcham.com/assets/images/84dfa30d.png" alt="course two" className="hover:saturate-50 rounded-2xl hover:scale-110 duration-150" /></div>
-              </figure>
-              <div className="card-body ">
-                <h2 className="card-title">Online course</h2>
-                <p>รู้เรื่องธุรกิจใน 3 นาที</p>
-                <div className="card-actions">
-                  <button className="btn bg-primary">Buy Now</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
 
 
 
         {/* Our work */}
-        <div className="mt-10">
-          <h2 className="text-2xl font-bold text-center py-5 pt-24">งานของเรา</h2>
-
-          <div className="grid gap-10 sm:flex justify-center">
-            <div className="card w-96 bg-base-100 shadow-xl">
-              <figure className="px-10 pt-10">
-                <div className="rounded-2xl overflow-hidden "><img src="https://www.choomcham.com/assets/images/84dfa30d.png" alt="course one" className="hover:saturate-50 rounded-2xl hover:scale-110 duration-150" /></div>
-              </figure>
-              <div className="card-body ">
-                <h2 className="card-title">Online course</h2>
-                <p>รู้เรื่องธุรกิจใน 3 นาที</p>
-                <div className="card-actions">
-                  <button className="btn bg-primary">Buy Now</button>
-                </div>
-              </div>
-            </div>
-
-            {/*  */}
-
-            <div className="card w-96 bg-base-100 shadow-xl">
-              <figure className="px-10 pt-10">
-                <div className="rounded-2xl overflow-hidden "><img src="https://www.choomcham.com/assets/images/84dfa30d.png" alt="course two" className="hover:saturate-50 rounded-2xl hover:scale-110 duration-150" /></div>
-              </figure>
-              <div className="card-body ">
-                <h2 className="card-title pt-24">Online course</h2>
-                <p>รู้เรื่องธุรกิจใน 3 นาที</p>
-                <div className="card-actions">
-                  <button className="btn bg-primary">Buy Now</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        
 
 
 
