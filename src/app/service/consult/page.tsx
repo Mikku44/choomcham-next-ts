1da347d6f41444fb1b1f0b3f1709ec0bba1546cf
Mikku44 <104062911+Mikku44@users.noreply.github.com>
2024-02-19 21:28:12 +0700

    Create page.tsx

---
+++ src/app/service/consult/page.tsx
@@ -0,0 +1,17 @@
+
+
+import Footer from '@/app/components/footer';
+import NavigationBar from '@/app/components/navbar';
+
+function Consult() {
+
+
+    return <>
+
+        <NavigationBar current="0" />
+        <section className="p-10"></section>
+        <Footer />
+    </>
+}
+
+export default Consult
