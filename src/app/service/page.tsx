4740ea255b7539c04a6245fd55f05ac5f85950e7
sulaimancheming <117164121+sulaimancheming@users.noreply.github.com>
2024-03-02 02:40:04 +0700

    res แล้วนิดหน่อย

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -38,64 +38,63 @@ function Service() {
 
 
             <div className="relative">
-                <motion.div initial={{ y: 20, opacity: 0 }} whileInView={{ y: 0, opacity: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1 }} className="">
-                    <div className="flex justify-start bg-gradient-to-l from-white via-white via-20% to-[#F2E6D7]  xl:pl-32 pl-1  ">
-                        <Image src="/images/kruEve_image4.jpg" className="xl:opacity-100 opacity-40 xl:w-[450px] w-[50vw]   blur-[0px]" alt="Description" width={1780} height={1280} />
+                <motion.div initial={{ y: 20, opacity: 0 }} whileInView={{ y: 0, opacity: 70 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1 }} className="">
+                    <div className="flex justify-start  bg-gradient-to-l from-white via-white via-20% to-[#F2E6D7]  xl:pl-32 pl-0  ">
+                        <Image src="/images/kruEve_image4.jpg" className="xl:opacity-100 opacity-60 xl:w-[450px] w-[250px]   blur-[0px]" alt="Description" width={1780} height={1280} />
                     </div>
 
-
-                    <div className="  text-start absolute w-[550px]  xl:top-[120px] top-[40px] xl:right-[250px] right-[10px] shadow-2xl bg-gradient-to-b from-white via-white via-20% to-[#F2E6D7]   p-5  ">
+                    <div className="  text-start absolute lg:w-[550px] w-[250px]   xl:top-[120px] top-[20px] xl:right-[250px] right-[10px] shadow-2xl bg-gradient-to-b from-white via-white via-20% to-[#F2E6D7] p-2 xl:p-5  ">
 
                         <p className="text-[#549deb] pl-5 xl:pb-6 font-bold xl:text-5xl text-lg">ชุ่มฉ่ำ Mindful Circle</p>
                         <p className="text-[#000000] pl-5 font-normal xl:text-2xl text-base text-wrap">
                             สังคมกัลยาณมิตรธุรกิจ <br className="xl:hidden" />
                             ไปด้วยกัน ไปได้ไกลฉ่ำ
                         </p>
-                        <div className="text-center  xl:pt-2 xl:text-xl text-sm bg-white border-2 border-[#D33666] xl:rounded-3xl rounded-xl xl:w-[240px] w-[100px]  xl:h-[60px] h-[20px] xl:mt-5 xl:mb-5 ">
-                            <p className="leading-[50px] text-[#17A78A] font-bold text-xl">ปัญหาของตัวจริงไม่มีที่ยืน</p>
+                        <div className="text-center  xl:pt-2 underline underline-offset-8  xl:mt-5 xl:mb-5 ">
+                            <p className="xl:leading-[50px] text-[#17A78A] font-bold xl:text-xl text-sm">ปัญหาของตัวจริงไม่มีที่ยืน</p>
                         </div>
                         <div className="text-[#000000] pl-5 xl:text-xl text-sm mb-5 mt-5  " >
                             <div className="flex gap-2 mb-3 mt-3">
                                 <span className="text-black font-bold">01</span>
-                                <span className="underline underline-offset-8">ไม่มั่นใจในคอนเทนต์ของตัวเอง</span>
+                                <span className="">ไม่มั่นใจในคอนเทนต์ของตัวเอง</span>
                             </div>
                             <div className="flex gap-2 mb-3 mt-3">
                                 <span className="text-black font-bold">02</span>
-                                <span className="underline underline-offset-8">เครียดง่ายเวลาเจอปัญหายอดตก</span>
+                                <span className="">เครียดง่ายเวลาเจอปัญหายอดตก</span>
                             </div>
                             <div className="flex gap-2 mb-3 mt-3">
                                 <span className="text-black font-bold">03</span>
-                                <span className="underline underline-offset-8">ไม่มีเพื่อนคุณภาพ อยากหา Connection
+                                <span className="">ไม่มีเพื่อนคุณภาพ อยากหา Connection
                                     <br />เพิ่มเติมเพื่อต่อยอด</span>
                             </div>
                             <div className="flex gap-2 mb-3 mt-3">
                                 <span className="text-black font-bold">03</span>
-                                <span className="underline underline-offset-8">ไม่มีไอเดีย รู้สึกตันๆกับตัวเอง</span>
+                                <span className="">ไม่มีไอเดีย รู้สึกตันๆกับตัวเอง</span>
                             </div>
                         </div>
-                        <button className="btn btn-outline btn-info btn-xs sm:btn-sm md:btn-md lg:btn-lg rounded-xl xl:w-[200px] w-[120px] bg-white  xl:text-xl text-xs " ><p className="text-[#17A78A]">สอบถามเพิ่มเติม</p></button>
+                        <button className="btn btn-outline btn-warning  btn-xs sm:btn-sm md:btn-md lg:btn-lg rounded-xl xl:w-[200px] w-[120px] bg-white  xl:text-xl text-xs " ><p className="text-[#17A78A]">สอบถามเพิ่มเติม</p></button>
                     </div>
                 </motion.div>
 
 
-                <div className="absolute xl:top-[0%] top-[50%] animate-pulse rotate-180 opacity-100">
-                    <Image src="/images/sulmanservices/icon 1.png" className="xl:w-[300px] w-[500px] " alt="Description" width={1780} height={1280} />
+                <div className="absolute xl:top-[0%] top-[1%] animate-pulse rotate-180 opacity-100">
+                    <Image src="/images/sulmanservices/icon 1.png" className="xl:w-[300px] w-[150px] " alt="Description" width={1780} height={1280} />
                 </div>
-                <div className=" absolute top-[60%] right-[5%]">
-                    <Image src="/images/sticker19.png" className="w-[20vw] " alt="Description" width={1780} height={1280} />
+                <div className=" absolute xl:top-[60%] top-[75%] right-[5%]">
+                    <Image src="/images/sticker19.png" className="xl:w-[20vw] w-[30vw]  " alt="Description" width={1780} height={1280} />
                 </div>
                 {/* <div className="absolute top-[22%] opacity-50 right-[14%] w-[550px] h-[450px] bg-[#F2E6D7] blur-[8px] rounded-3xl "></div> */}
             </div>
-
-            <div className="flex justify-around xl:pt-11 xl:pb-7 h-auto w-[100%] bg-[#Ffff] border-5 border-[#F2E6D7] rounded-3xl ">
+            <div className="block md:hidden w-full h-10"></div>
+            <div className="flex justify-around  xl:pt-11 xl:pb-7 h-auto w-[100%] bg-[#Ffff] border-5 border-[#F2E6D7] rounded-3xl ">
                 {/* <button className="btn bg-black w-[200px] h-10 text-white text-center text-lg rounded-3xl"><p>หลักสูตรเจ้าของธุรกิจ</p></button>
                 <button className="btn bg-black w-[200px] h-10 text-white text-center text-lg rounded-3xl"><p>สำหรับงานโปรดักชั่น</p></button>
                 <button className="btn bg-black w-[200px] h-10 text-white text-center text-lg rounded-3xl"><p>คอร์สออนไลน์ </p></button> */}
-                <div className=" ">
+                <div className="">
                     <p className="xl:text-3xl text-lg font-bold">
                         ชุ่มฉ่ำ Circle คือ
                     </p>
-                    <span className="text-[#070814] xl:text-3xl text-xs">
+                    <span className="text-[#070814] xl:text-3xl text-sm">
                         กิจกรรม Mini Workshop เจอตัวประจำเดือน ให้นักธุรกิจตัวจริงมาเจอกัน<br className="xl:hidden" />
                         เพื่อฝึกสติให้ชุ่มฉ่ำจะได้สร้างแบรนด์สำเร็จอย่างมีความสุข <br />
                         ช่วยเหลือแบ่งปันเพื่อนธุรกิจให้สร้างแบรนด์ส่งต่อคุณค่าได้เพิ่มขึ้น
@@ -106,26 +105,29 @@ function Service() {
         </section >
 
         <section>
-            <div className="relative  bg-gradient-to-b from-white to-[#F2E6D7]  pt-10 pr-10 h-[800px]">
-                <div className="w-[75vw] h-[200px]  bg-white rounded-2xl shadow-2xl  z-10">
-                    <div className="text-2xl text-balance text-center pl-10 pt-10">
-                        <span >ชุ่มฉ่ำอยากให้คุณสร้างแบรนด์ได้สำเร็จอย่างยั่งยืน
-                            ระหว่างทางจึงสำคัญไม่แพ้การตั้งเป้าหมาย<br />
-                            การสร้างแบรนด์ให้สำเร็จเป็นการเดินทางไกล
-                            เพื่อส่งต่อคุณค่าในระยะยาว
-                            มีเพื่อนที่ดีจะทำให้คุณไม่หลงทาง
-                        </span>
+            <div className="relative  bg-gradient-to-b from-white to-[#F2E6D7]  pt-10 xl:pr-10 h-[800px]">
+                <div className="flex justify-start">
+                    <div className="w-[75vw] h-[200px]  bg-white rounded-2xl shadow-2xl  ">
+                        <div className=" xl:pl-10 pt-10 p-2">
+                            <span className="xl:text-2xl text-base xl:text-center">
+                                ชุ่มฉ่ำอยากให้คุณสร้างแบรนด์ได้สำเร็จอย่างยั่งยืน
+                                ระหว่างทางจึงสำคัญไม่แพ้การตั้งเป้าหมาย<br className="lg:hidden" />
+                                การสร้างแบรนด์ให้สำเร็จเป็นการเดินทางไกล
+                                เพื่อส่งต่อคุณค่าในระยะยาว
+                                มีเพื่อนที่ดีจะทำให้คุณไม่หลงทาง
+                            </span>
+                        </div>
                     </div>
                 </div>
-                <div className="absolute top-[20%] animate-pulse right-[20%]">
-                    <Image src="/icon.svg" className="w-[10vw] " alt="Description" width={1780} height={1280} />
+                <div className="absolute xl:top-[20%] top-[23%] animate-pulse xl:right-[20%] right-[4%] ">
+                    <Image src="/icon.svg" className="xl:w-[10vw] w-[20vw] " alt="Description" width={1780} height={1280} />
                 </div>
-                <div className="absolute top-[5.5%]  left-[0%]">
+                <div className="absolute hidden md:block top-[5.5%]  left-[0%]">
                     <Image src="/masker.svg" className="w-[10vw] " alt="Description" width={1780} height={1280} />
                 </div>
                 <div className="flex justify-end pt-20">
-                    <div className="w-[95vw] h-[350px] bg-white  rounded-2xl shadow-2xl z-0">
-                        <div className="text-2xl text-balance text-start gap-3  pl-[550px] pt-10">
+                    <div className="xl:w-[95vw] w-[70vw] h-[350px] bg-white  rounded-2xl shadow-2xl z-0">
+                        <div className="xl:text-2xl text-start gap-3  xl:pl-[550px] xl:pt-10 pt-5 pl-2">
                             <div>
                                 <p className="font-bold">รายละเอียด</p>
                             </div>
@@ -158,13 +160,10 @@ function Service() {
                             </div>
                         </div>
                     </div>
-                    {/* <motion.div initial={{ opacity: 0, y: -100, scale: 0.5 }}
-                        animate={{ opacity: 1, y: 0, scale: 1 }}
-                        transition={{ duration: 1 }} > */}
-                    <div className="absolute left-[10%]  top-[25%] opacity-95 z-10">
-                        <Image src="/images/kruEve_image3.jpg" className="w-[20vw] " alt="Description" width={1780} height={1280} />
+
+                    <div className="absolute left-[10%]  xl:top-[25%] top-[26%] opacity-95 z-10">
+                        <Image src="/images/kruEve_image3.jpg" className="xl:w-[20vw] w-[150px] " alt="Description" width={1780} height={1280} />
                     </div>
-                    {/* </motion.div> */}
 
                 </div>
             </div>
@@ -174,7 +173,7 @@ function Service() {
 
                 <div className="w-[25vw] h-[250px] bg-gradient-to-l from-white via-[#F2E6D7] via-100% to-[#F2E6D7] rounded-2xl ">
                     <div className="text-xl text-balance text-start pl-10 pt-10">
-                        <p className="font-bold">เหมาะกับใครบ้าง? </p>
+                        <p className="font-bold ">เหมาะกับใครบ้าง? </p>
                         <a>- เจ้าของธุรกิจที่เพิ่งเริ่ม</a><br />
                         <a>- เจ้าของธุรกิจที่ต้องการหา Connection</a><br />
                         <a>- เจ้าของธุรกิจที่สำเร็จแล้วอยากแบ่งปัน</a>
